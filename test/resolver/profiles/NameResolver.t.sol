c83ca4fb0a1123e45905ed0a55d08139167161ae
Lethale <lethale@berakin.io>
2024-10-14 14:16:54 +0200

    solady plus nameWithEmojis

---
+++ test/resolver/profiles/NameResolver.t.sol
@@ -0,0 +1,49 @@
+// test/TestNameResolver.t.sol
+pragma solidity >=0.8.4;
+
+import {Test} from "forge-std/Test.sol";
+import {NameResolver} from "src/resolver/profiles/NameResolver.sol";
+
+// Dummy implementation of NameResolver for testing
+contract TestNameResolver is NameResolver {
+    constructor() NameResolver() {}
+
+    function isAuthorized(bytes32) internal pure virtual returns (bool) {
+        return true;
+    }
+
+    function isAuthorised(bytes32) internal view virtual override returns (bool) {
+        return true;
+    }
+}
+
+contract TestNameResolverTest is Test {
+    TestNameResolver resolver;
+
+    function setUp() public {
+        resolver = new TestNameResolver();
+    }
+
+    function testGetEmptyName() public view {
+        bytes32 node = keccak256("example");
+
+        string memory retrievedName = resolver.name(node);
+        assertEq(retrievedName, "");
+    }
+
+    function testSetName() public {
+        bytes32 node = keccak256("example");
+        resolver.setName(node, "example.bera");
+
+        string memory retrievedName = resolver.name(node);
+        assertEq(retrievedName, "example.bera");
+    }
+
+    function testGetNameWithEmojis() public {
+        bytes32 node = keccak256("example");
+        resolver.setName(node, "example.bera");
+
+        string memory retrievedName = resolver.nameWithEmojis(node);
+        assertEq(retrievedName, unicode"example.🐻⛓️");
+    }
+}
