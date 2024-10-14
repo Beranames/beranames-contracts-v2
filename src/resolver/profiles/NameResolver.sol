ff04ad829a2c9b8cb0b509875a5312d7be5bd03c
Lethale <lethale@berakin.io>
2024-10-14 14:16:54 +0200

    solady plus nameWithEmojis

--- src/resolver/profiles/NameResolver.sol
+++ src/resolver/profiles/NameResolver.sol
@@ -3,8 +3,13 @@ pragma solidity >=0.8.4;
 
 import {ResolverBase} from "src/resolver/types/ResolverBase.sol";
 import "src/resolver/interfaces/INameResolver.sol";
+import {LibString} from "lib/solady/src/utils/LibString.sol";
+
+import {console} from "forge-std/console.sol";
 
 abstract contract NameResolver is INameResolver, ResolverBase {
+    using LibString for string;
+
     mapping(uint64 => mapping(bytes32 => string)) versionable_names;
 
     /**
@@ -12,10 +17,7 @@ abstract contract NameResolver is INameResolver, ResolverBase {
      * May only be called by the owner of that node in the BNS registry.
      * @param node The node to update.
      */
-    function setName(
-        bytes32 node,
-        string calldata newName
-    ) external virtual authorised(node) {
+    function setName(bytes32 node, string calldata newName) external virtual authorised(node) {
         versionable_names[recordVersions[node]][node] = newName;
         emit NameChanged(node, newName);
     }
@@ -26,17 +28,16 @@ abstract contract NameResolver is INameResolver, ResolverBase {
      * @param node The BNS node to query.
      * @return The associated name.
      */
-    function name(
-        bytes32 node
-    ) external view virtual override returns (string memory) {
+    function name(bytes32 node) external view virtual override returns (string memory) {
         return versionable_names[recordVersions[node]][node];
     }
 
-    function supportsInterface(
-        bytes4 interfaceID
-    ) public view virtual override returns (bool) {
-        return
-            interfaceID == type(INameResolver).interfaceId ||
-            super.supportsInterface(interfaceID);
+    function nameWithEmojis(bytes32 node) external view virtual returns (string memory) {
+        string memory nameWithoutEmojis = this.name(node);
+        return nameWithoutEmojis.replace(".bera", unicode".🐻⛓️");
+    }
+
+    function supportsInterface(bytes4 interfaceID) public view virtual override returns (bool) {
+        return interfaceID == type(INameResolver).interfaceId || super.supportsInterface(interfaceID);
     }
 }
